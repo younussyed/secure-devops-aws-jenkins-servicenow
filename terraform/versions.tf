cc475d6bbe59700f32f80db459f1566ed075a3a5
younussyed <younus.syed97@gmail.com>
2026-01-17 07:00:35 -0600

    Add Terraform backend (S3 + DynamoDB) and AWS provider

---
+++ terraform/versions.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "REPLACE_WITH_YOUR_BUCKET_NAME"
+    key            = "secure-devops/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
