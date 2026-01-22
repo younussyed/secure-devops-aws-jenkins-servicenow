adfbce6da2aa962210d0d09135eb1efa82884628
younussyed <younus.syed97@gmail.com>
2026-01-22 03:19:11 -0600

    add subnet outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,12 @@
 output "vpc_id" {
   value = aws_vpc.main.id
 }
+
+output "public_subnet_id" {
+  value = aws_subnet.public_a.id
+}
+
+output "private_subnet_id" {
+  value = aws_subnet.private_a.id
+}
+
